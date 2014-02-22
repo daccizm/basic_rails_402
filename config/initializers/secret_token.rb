eee20304dc0d86191febfe742f7e82ce42203fb2
Hiroyuki Fukuda <fukuishere@gmail.com>
2014-02-23 00:05:00 +0900

    .gitignoreからsecret_tokenを除外

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BasicApp::Application.config.secret_key_base = '86cc881b9b01ee79ff3b7bacc47293fe46e1412c6f90854f83ae103d37cf64a9a7d1e655ed0bae726395276dd0a54dd4399e60f826b44ae55b050cbecbd8d4e7'
